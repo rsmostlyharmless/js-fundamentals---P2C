e2af7d8ef0754ec019d994e58915455309b067e9
Ryan.Syder <rsmostlyharmless@gmail.com>
2022-09-14 15:55:20 +0200

    Challnge 4 complete. Needed video for bonus

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-`use strict`
+`use strict`;
 
 // Challenge 1
 /* Your tasks:
@@ -28,10 +28,10 @@ Test data:
 //     }
 // }
 // checkWinner(dolphins, koalas);
-
-
-
-
+//
+//
+//
+//
 // Challenge 2
 // Steven is still building his tip calculator, using the same rules as before: Tip 15% of the bill if the bill value is between 50 and 300, and if the value is different, the tip is 20%.
 
@@ -51,10 +51,10 @@ Test data:
 
 // console.log(tips);
 // console.log(total);
-
-
-
-
+//
+//
+//
+//
 // Challenge 3
 /* Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter).
 
@@ -65,35 +65,74 @@ Your tasks:
 
 Test data: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall. */
 
-const mark = {
-    FullName: `Mark Miller`,
-    mass: 78,
-    height: 1.69,
-    calcBMI: function() {
-        return mark.mass / mark.height ** 2;
-    } 
+// const mark = {
+//     FullName: `Mark Miller`,
+//     mass: 78,
+//     height: 1.69,
+//     calcBMI: function() {
+//         return mark.mass / mark.height ** 2;
+//     }
+// }
+
+// const john = {
+//     FullName: `John Smith`,
+//     mass: 92,
+//     height: 1.95,
+//     calcBMI: function() {
+//         return john.mass / (john.height * john.height);
+//     }
+// }
+
+// const highLow = () => mark.calcBMI() > john.calcBMI() ? `higher` : `Lower`;
+
+// console.log(`${mark.FullName}'s BMI (${mark[`calcBMI`]()}) is ${highLow()} than ${john.FullName}'s (${john[`calcBMI`]()})!`);
+
+// if (mark.calcBMI() > john.calcBMI()) {
+//     console.log(`${mark.FullName}'s BMI (${mark[`calcBMI`]()}) is higher than ${john.FullName}'s (${john[`calcBMI`]()})!`);
+// } else {
+//     console.log(`${john.FullName}'s BMI (${john[`calcBMI`]()}) is higher than ${mark.FullName}'s (${mark[`calcBMI`]()})!`);
+// }
+//
+//
+//
+//
+// Challenge 4
+// Let's improve Steven's tip calculator even more, this time using loops!
+// Your tasks:
+
+// 1. Create an array 'bills' containing all 10 test bill values
+// 2. Create empty arrays for the tips and the totals ('tips' and 'totals')
+// 3. Use the 'calcTip' function we wrote before (no need to repeat) to calculate tips and total values (bill + tip) for every bill value in the bills array. Use a for loop to perform the 10 calculations!
+// Test data: 22, 295, 176, 440, 37, 105, 10, 1100, 86 and 52
+
+// Hints: Call ‘calcTip ‘in the loop and use the push method to add values to the tips and totals arrays 😉
+
+// Bonus:
+// 4. Bonus: Write a function 'calcAverage' which takes an array called 'arr' as an argument. This function calculates the average of all numbers in the given array. This is a difficult challenge (we haven't done this before)! Here is how to solve it:
+// 4.1. First, you will need to add up all values in the array. To do the addition, start by creating a variable 'sum' that starts at 0. Then loop over the array using a for loop. In each iteration, add the current value to the 'sum' variable. This way, by the end of the loop, you have all values added together.
+// 4.2. To calculate the average, divide the sum you calculated before by the length of the array (because that's the number of elements).
+// 4.3. Call the function with the 'totals' array.
+
+let bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+let tips = [];
+let totals = [];
+
+const calcTip = (bill) => (bill > 50 && bill < 300 ? (bill * 15) / 100 : (bill * 20) / 100);
+
+for (let i = 0; i < bills.length; i++) {
+    tips.push(calcTip(bills[i]));
+    totals.push(bills[i] + tips[i]);
 }
 
-const john = {
-    FullName: `John Smith`,
-    mass: 92,
-    height: 1.95,
-    calcBMI: function() {
-        return john.mass / (john.height * john.height);
-    } 
-}
+const calcAverage = function (arr) {
+    let sum = 0;
 
-const highLow = () => mark.calcBMI() > john.calcBMI() ? `higher` : `Lower`;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i];
+    }
+    return sum / arr.length;
+};
+const avg = calcAverage(totals);
 
-console.log(`${mark.FullName}'s BMI (${mark[`calcBMI`]()}) is ${highLow()} than ${john.FullName}'s (${john[`calcBMI`]()})!`);
-
-if (mark.calcBMI() > john.calcBMI()) {
-    console.log(`${mark.FullName}'s BMI (${mark[`calcBMI`]()}) is higher than ${john.FullName}'s (${john[`calcBMI`]()})!`);
-} else {
-    console.log(`${john.FullName}'s BMI (${john[`calcBMI`]()}) is higher than ${mark.FullName}'s (${mark[`calcBMI`]()})!`);
-}
-
-
-
-
-// Challenge 5
+console.log(bills, tips, totals);
+console.log(avg);
